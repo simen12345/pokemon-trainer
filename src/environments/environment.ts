3d2e23210fd53b73e8600cf29a0e40fe161b8b09
Gøran Helle <goranolav98@gmail.com>
2023-02-28 13:41:00 +0100

    Added images to pokemons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: false,
     apiTrainers: "https://swamp-cyclic-jumpsuit.glitch.me/trainers",
     apiPokemon: "https://pokeapi.co/api/v2/pokemon",
-    apiKey: "noroff"
+    apiKey: "noroff", 
+    apiImg: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+
 }
