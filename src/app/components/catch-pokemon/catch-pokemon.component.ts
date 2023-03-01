a63af3829d661d8a52a7dad760633cad38b5f9b9
Gøran Helle <goranolav98@gmail.com>
2023-03-01 10:54:11 +0100

    Added icon to show caught pokemon

--- src/app/components/catch-pokemon/catch-pokemon.component.ts
+++ src/app/components/catch-pokemon/catch-pokemon.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { Trainer } from 'src/app/models/trainer.model';
 import { CaughtPokemonService } from 'src/app/services/caught-pokemon.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 @Component({
   selector: 'app-catch-pokemon',
@@ -10,18 +11,25 @@ import { CaughtPokemonService } from 'src/app/services/caught-pokemon.service';
 })
 export class CatchPokemonComponent  implements OnInit{
 
+   public isCaught:boolean=false; 
   @Input() pokemonName: string = "";
  
   get loading(): boolean{
     return this. caughtService.loading;
   }
+
+  
   constructor(
+    private trainerService : TrainerService,
     private readonly caughtService:CaughtPokemonService
   ){ }
  
   ngOnInit(): void {   
+    this.isCaught = this.trainerService.inCaught(this.pokemonName);
+    
  }
  onCatchClick(): void{
+  alert("You just caught a pokemon")
   this.caughtService.addToCaught(this.pokemonName)
   .subscribe({
     next: (response:Trainer) => {
